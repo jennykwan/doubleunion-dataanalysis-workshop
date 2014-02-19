360e5f24d309b694be4a7da1b50b637b11df4e33
Alyssa Kwan <me@alyssackwan.name>
2014-02-19 15:20:54 -0800

    lein new

---
+++ src/doubleunion_dataanalysis_workshop/core.clj
@@ -0,0 +1,7 @@
+(ns doubleunion-dataanalysis-workshop.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
---
+++ test/doubleunion_dataanalysis_workshop/core_test.clj
@@ -0,0 +1,7 @@
+(ns doubleunion-dataanalysis-workshop.core-test
+  (:require [clojure.test :refer :all]
+            [doubleunion-dataanalysis-workshop.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
