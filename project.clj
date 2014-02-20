d661c73c069a537dfda71645130eaeb8e85f004d
Alyssa Kwan <me@alyssackwan.name>
2014-02-19 16:22:20 -0800

    deps ring and compojure

--- project.clj
+++ project.clj
@@ -1,9 +1,11 @@
 (defproject doubleunion-dataanalysis-workshop "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Data Analysis and Visualization with Clojure / Incanter and ClojureScript / D3"
+  :url "http://alyssackwan.github.io/doubleunion-dataanalysis-workshop/"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [ring "1.2.1"]
+                 [compojure "1.1.6"]]
   :main ^:skip-aot name.alyssackwan.census.main
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
